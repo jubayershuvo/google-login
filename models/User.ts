7208a0c03c3eaae7ed6b04ed4b9ab25346ec3072
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-09-24 15:39:00 +0600

    wesd

---
+++ models/User.ts
@@ -0,0 +1,17 @@
+// models/User.ts
+
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    photoUrl: { type: String },
+    name: { type: String },
+    provider: { type: String },
+    providerAccountId: { type: String },
+    refreshToken: { type: String },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
