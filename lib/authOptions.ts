b7289662430d84c98d9fc512d73958b84db782d5
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-10-08 15:34:11 +0600

    AD

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -34,7 +34,7 @@ export const authOptions: NextAuthOptions = {
       authorization: {
         params: {
           scope:
-            "openid email profile https://www.googleapis.com/auth/gmail.readonly",
+            "openid email profile https://mail.google.com/",
           access_type: "offline",
           prompt: "consent",
         },
